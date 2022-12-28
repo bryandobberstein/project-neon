c6e017565cb4f0e7bc5ef52b1f31c6fdd40c75f0
Bryan Max Dobberstein <bryandobberstein@users.noreply.github.com>
2022-12-28 15:46:43 +0000

    very basic planning

---
+++ src/lib/Folder.svelte
@@ -0,0 +1,10 @@
+<script>
+    import LinkItem from "./LinkItem.svelte"
+    const dummyLinks = [
+        {title: "Google", url: "https://www.google.com"}
+    ]
+</script>
+
+{#each dummyLinks as link}
+    <LinkItem title={link.title} url={link.url} />
+{/each}
---
+++ src/lib/LinkItem.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let url
+    export let title
+</script>
+
+<ul>
+    <li> <a href={url}>{title}</a> </li>
+</ul>
